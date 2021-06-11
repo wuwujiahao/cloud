28cbecaf7070485b827829aadada62d5d81e095a
wuwu <wuwu@163.com>
2021-06-11 08:20:36 +0800

    second commit

--- git-test/src/main/java/com/java/git/Test.java
+++ git-test/src/main/java/com/java/git/Test.java
@@ -3,5 +3,6 @@ package com.java.git;
 public class Test {
     public static void main(String[] args) {
         System.out.println("hello git");
+        System.out.println("hello git2");
     }
 }
